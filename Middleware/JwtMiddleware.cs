4fc526c0cbe36e4614b1279ec05df641e8a5f313
Nkanyezi <nkatso98@gmail.com>
2025-09-03 17:58:08 +0200

    Update JwtMiddleware.cs

--- Middleware/JwtMiddleware.cs
+++ Middleware/JwtMiddleware.cs
@@ -3,16 +3,7 @@ namespace PscTechBackend.Middleware
     public sealed class JwtMiddleware
     {
         private readonly RequestDelegate _next;
-
-        public JwtMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public async Task InvokeAsync(HttpContext context)
-        {
-            // If you had token validation logic, place it here.
-            await _next(context);
-        }
+        public JwtMiddleware(RequestDelegate next) { _next = next; }
+        public Task InvokeAsync(HttpContext context) => _next(context);
     }
 }
